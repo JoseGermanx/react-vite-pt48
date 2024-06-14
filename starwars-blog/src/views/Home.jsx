25a00c005cbd81a173f714aeecc4a8667fe3b1b2
Jose Germán Martínez <jgermanmm@gmail.com>
2024-06-14 09:19:43 -0400

    feat: Mejorar carga de imágenes en el componente Card
    Este commit mejora la carga de imágenes en el componente Card. Se agrega la propiedad "h-100" a la clase "card" para asegurar que la tarjeta ocupe toda la altura disponible. Además, se actualiza la URL de la imagen para utilizar un placeholder genérico en lugar de una imagen vacía. Estos cambios mejoran la presentación visual y la experiencia del usuario al cargar las imágenes de forma más eficiente.

--- starwars-blog/src/views/Home.jsx
+++ starwars-blog/src/views/Home.jsx
@@ -1,17 +1,25 @@
 import ListCard from "../components/ListCard";
-import { useContext, useEffect} from "react";
+import { useContext, useEffect } from "react";
 import GlobalContext from "../context/GlobalContext";
+import starwars from "../assets/sw.svg";
 
 const Home = () => {
   const { global, globalActions } = useContext(GlobalContext);
-  
+
   useEffect(() => {
     globalActions({ type: "loadPeople" });
   }, []);
 
   return (
-    <div className="container" style={{marginTop: "110px"}}>
-      <h1 className="text-center">Star Wars Blog</h1>
+    <div className="container" style={{ marginTop: "70px" }}>
+      <div className="container d-flex justify-content-center">
+        <img
+          src={starwars}
+          alt=""
+          width="200"
+          className="align-text-top"
+        />
+      </div>
       <ListCard apiFetch={global.planets} type={"planets"} />
       <ListCard apiFetch={global.people} type={"people"} />
       <ListCard apiFetch={global.vehicles} type={"vehicles"} />
